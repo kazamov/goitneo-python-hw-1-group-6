28042a763cea85ece2c5b9a394af0c2bf4af4dc4
Zakir Nuriiev <zakir.nuriiev@gmail.com>
2023-09-30 21:28:41 +0300

    implement "get_birthdays_per_week" function

---
+++ home_task_1/task_1.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+
+def get_birthdays_per_week(users: list[dict]):
+    """Print users who have birthday in current week."""
+    if len(users) == 0:
+        print("No users found")
+        return
+
+    birthdays_list = defaultdict(list)
+    current_date = datetime.now().date()
+
+    for user in users:
+        name = user["name"]
+        birthday = user["birthday"].date()
+        birthday_this_year = birthday.replace(year=current_date.year)
+
+        if birthday_this_year < current_date:
+            birthday_this_year = birthday_this_year.replace(year=current_date.year + 1)
+
+        if birthday_this_year.weekday() == 5:
+            birthday_this_year += timedelta(days=2)
+        elif birthday_this_year.weekday() == 6:
+            birthday_this_year += timedelta(days=1)
+
+        delta_days = (birthday_this_year - current_date).days
+        if delta_days > 0 and delta_days <= 7:
+            birthdays_list[birthday_this_year].append(name)
+
+    if len(birthdays_list) == 0:
+        print("No birthdays near 7 days")
+
+    sorted_birthdays_list = sorted(birthdays_list.keys())
+    for day in sorted_birthdays_list:
+        print(f"{day.strftime('%A')}: {', '.join(birthdays_list[day])}")
+
+
+# --------------------------------------------------------------------
+
+""" USERS = [
+    {"name": "Zakir Nuriiev", "birthday": datetime(1992, 10, 7)},
+    {"name": "Liudmila Nuriieva", "birthday": datetime(1989, 10, 8)},
+    {"name": "Rustam Nuriiev", "birthday": datetime(2016, 10, 3)},
+    {"name": "Alisa Nuriieva", "birthday": datetime(2021, 10, 4)},
+    {"name": "Shamil Nuriiev", "birthday": datetime(1999, 10, 5)},
+    {"name": "Tetiana Nuriieva", "birthday": datetime(1972, 10, 6)},
+]
+
+EMPTY_USERS = []
+
+get_birthdays_per_week(EMPTY_USERS) """
